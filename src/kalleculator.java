8306b3eb62329d5c5b34492fbf5a7975171aba56
JonathanLissman <facehump90@gmail.com>
2021-03-20 15:17:30 +0100

    Added method to extract numbers from string, add new delimiters, and summarise numbers, and tests. Step 3

--- src/kalleculator.java
+++ src/kalleculator.java
@@ -1,21 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static java.lang.Integer.parseInt;
 
 public class kalleculator {
+    ArrayList<String> delimiters = new ArrayList<>(Arrays.asList(",","\n"));
+
+    public int addDelims(String... newDelimiters) {
+        int addedCount = 0;
+        for (String delimiter:newDelimiters) {
+            System.out.println("Adding delimiter with length of "+delimiter.length());
+            delimiters.add(delimiter);
+            addedCount++;
+        }
+        System.out.println("Delimiters are now :");
+        for (String delim:delimiters
+        ) {
+            System.out.println(delim);
+        }
+        System.out.println("-------------------");
+        return(addedCount);
+    }
+
+
+    public ArrayList<Integer> getNumbersFromString(String inputNumbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (String delim:delimiters
+        ) {
+            inputNumbers = inputNumbers.replace(delim,"|");
+
+        }
+        String[] rawSults = inputNumbers.split("|");
+        for (String resource :rawSults
+        ) {
+            if (!resource.equals("|")){
+                result.add(parseInt(resource));
+            }
+        }
+        return result;}
 
 
 
     int add(String s){
-        String[] nums = s.split(",");
-        int parsedNum = 0;
+        ArrayList<Integer> nums = new ArrayList();
         int sum = 0;
-        for (String num:nums
-             ) {
-            if(!num.equals("")){
-                parsedNum = parseInt(num);
-                sum = sum+parsedNum;
-            }
-        }
+        
+        nums = getNumbersFromString(s);
+        sum = sumNum(nums);
+
         return sum;
     }
 
+    public Integer sumNum(ArrayList<Integer> numbers) {
+        int sum=0;
+        for (Integer n:numbers
+        ) {
+            sum = sum+n;
+        }
+        return sum;
+    }
 }
--- tests/kalleculatorTest.java
+++ tests/kalleculatorTest.java
@@ -1,5 +1,8 @@
 import jdk.swing.interop.SwingInterOpUtils;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,9 +13,9 @@ class kalleculatorTest {
         kalleculator calc = new kalleculator();
 
         int actualValue = calc.add("1,2");
-        int expectedValue = 6;
+        int expectedValue = 3;
 
-        assertEquals(actualValue,expectedValue);
+        assertEquals(expectedValue,actualValue);
 
     }
     @Test
@@ -20,26 +23,102 @@ class kalleculatorTest {
         kalleculator calc = new kalleculator();
         Random rng = new Random();
         String testNumbers = "";
-        int expectedResult = 0;
+        int expectedValue = 0;
         int rotations = rng.nextInt(20);
         System.out.println("Testing with " + rotations + " numbers");
         for (int i = 0; i < rotations; i++) {
-            //dont add last comma
+
             if (i == rotations - 1) {
+                //dont add last comma to string
                 int ranNum = rng.nextInt(9);
                 testNumbers = testNumbers + ranNum;
-                expectedResult+=ranNum;
+                expectedValue+=ranNum;
             }else{
                 int ranNum = rng.nextInt(9);
                 testNumbers = testNumbers + ranNum + ",";
-                expectedResult+=ranNum;
+                expectedValue+=ranNum;
             }}
         System.out.println("Argument string is now "+testNumbers);
+        System.out.println("Expected result is "+expectedValue);
             int actualValue = calc.add(testNumbers);
 
-            assertEquals(actualValue,expectedResult);
+            assertEquals(expectedValue,actualValue);
         }
 
 
+        @Test
+        void testNewDelim(){
+        kalleculator calc = new kalleculator();
+
+        int actualValue = calc.add("1,2\n3");
+        int expectedValue = 6;
+
+        assertEquals(expectedValue,actualValue);
+
+
+
+
+
+        }
+
+        @Test
+        void testgetNumbersFromString(){
+        kalleculator calc = new kalleculator();
+
+        ArrayList<Integer> actualResult = calc.getNumbersFromString("1,2\n3");
+        //we expect that the length of the arraylist is 3.
+        int expectedResult = 3;
+        //
+        assertEquals(expectedResult,actualResult.size());
+
+
+        }
+
+        @Test
+        void addNewDelimsTest(){
+            kalleculator calc = new kalleculator();
+
+            //return numbers of added delims
+            int actualResult = calc.addDelims(";");
+            int expectedResult = 1;
+
+            assertEquals(expectedResult,actualResult);
+
+        }
+        @Test
+        void addMultipleNewDelims(){
+            kalleculator calc = new kalleculator();
+
+            //returns numbers of added delims
+            int actualResult = calc.addDelims(";","K","d");
+            int expectedResult = 3;
+
+            assertEquals(expectedResult,actualResult);
+
+        }
+        @Test
+        void addLongerNewDelimiter(){ kalleculator calc = new kalleculator();
+
+            //returns numbers of added delims
+            int actualResult = calc.addDelims(";;");
+            int expectedResult = 1;
+
+            assertEquals(expectedResult,actualResult);}
+
+
+
+
+
+        @Test
+        void sumNumTest(){
+            kalleculator calc = new kalleculator();
+            ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3));
+            //returns numbers of added delims
+            int actualResult = calc.sumNum(numbers);
+            int expectedResult = 6;
+
+            assertEquals(expectedResult,actualResult);
+
+        }
     }
 
