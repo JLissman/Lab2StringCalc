8ee6f3ee9784bbad16acb0d306693e9961996461
JonathanLissman <facehump90@gmail.com>
2021-03-20 14:38:24 +0100

    added test for unlimited amount of numbers, Step 2

--- tests/kalleculatorTest.java
+++ tests/kalleculatorTest.java
@@ -1,17 +1,45 @@
+import jdk.swing.interop.SwingInterOpUtils;
 import org.junit.jupiter.api.Test;
-
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class kalleculatorTest {
 
     @Test
-    void add() {
+    void addTest() {
         kalleculator calc = new kalleculator();
 
-        int actualValue = calc.add("1,2,3");
+        int actualValue = calc.add("1,2");
         int expectedValue = 6;
 
         assertEquals(actualValue,expectedValue);
 
     }
-}
+    @Test
+    void testRandomAmountOfNumbers(){
+        kalleculator calc = new kalleculator();
+        Random rng = new Random();
+        String testNumbers = "";
+        int expectedResult = 0;
+        int rotations = rng.nextInt(20);
+        System.out.println("Testing with " + rotations + " numbers");
+        for (int i = 0; i < rotations; i++) {
+            //dont add last comma
+            if (i == rotations - 1) {
+                int ranNum = rng.nextInt(9);
+                testNumbers = testNumbers + ranNum;
+                expectedResult+=ranNum;
+            }else{
+                int ranNum = rng.nextInt(9);
+                testNumbers = testNumbers + ranNum + ",";
+                expectedResult+=ranNum;
+            }}
+        System.out.println("Argument string is now "+testNumbers);
+            int actualValue = calc.add(testNumbers);
+
+            assertEquals(actualValue,expectedResult);
+        }
+
+
+    }
+
